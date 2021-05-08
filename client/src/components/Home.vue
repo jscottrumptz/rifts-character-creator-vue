6482f1614f6c195bf398a6f800972b202eaa3bf1
J Scott Rumptz <jscott@rumptz.tech>
2021-05-08 12:45:46 -0500

    login cleanup

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <h2 class="text-2xl font-bold leading-7 text-center text-white sm:text-3xl sm:truncate p-6">
-      Home Page
-    </h2>
-
     <Login v-if="!loggedIn"/>
     <PickRace/>
     <PickPsionics/>
